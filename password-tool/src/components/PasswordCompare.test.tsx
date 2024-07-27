38e905c21ac3968cff2ad1156631b76851885995
szymg2001 <szymon.grabowiec2001@gmail.com>
2024-07-27 15:37:30 +0200

    Fixed building error

--- password-tool/src/components/PasswordCompare.test.tsx
+++ password-tool/src/components/PasswordCompare.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PasswordCompare from "./PasswordCompare";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 describe("Component - PasswordCompare", () => {
